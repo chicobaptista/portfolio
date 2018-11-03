f691a394af258b27cfcb2f5fd19dbf2a242e5650
Francisco Baptista <chico.baptista@gmail.com>
2018-11-03 20:26:36 -0300

    Create boilerplate components

---
+++ src/components/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Contact works!</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Contact extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/TopMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+   <h2>TopMenu Works!</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TopMenu extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
