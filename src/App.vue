1ba6eb2d3fa30d73e58ce9d4fa3d5728eb3721f4
Francisco Baptista <chico.baptista@gmail.com>
2018-11-03 21:53:20 -0300

    add intro component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <TopMenu />
+    <Intro />
     <Projects />
     <Skills />
     <Bio />
@@ -16,6 +17,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import HelloWorld from './components/HelloWorld.vue';
 import TopMenu from './components/TopMenu.vue';
+import Intro from './components/Intro.vue';
 import Projects from './components/Projects.vue';
 import Skills from './components/Skills.vue';
 import Bio from './components/Bio.vue';
@@ -25,6 +27,7 @@ import Contact from './components/Contact.vue';
   components: {
     HelloWorld,
     TopMenu,
+    Intro,
     Projects,
     Skills,
     Bio,
@@ -42,5 +45,9 @@ export default class App extends Vue {}
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  .uppercase {
+    text-transform: uppercase;
+  }
 }
 </style>
