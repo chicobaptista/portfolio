e041460fccf88d19d15431ce8e7c13a3295663f0
Francisco Baptista <chico.baptista@gmail.com>
2018-11-18 17:59:05 -0200

    add vue-i18n dependency

---
+++ src/config/i18n.js
@@ -0,0 +1,29 @@
+export const defaultLocale = 'pt'
+ 
+export const locales = [
+    {
+        code: 'en',
+        name: 'English',
+        dir: 'ltr',
+    },
+    {
+        code: 'pt',
+        name: 'Português',
+        dir: 'ltr',
+    },
+    {
+        code: 'es',
+        name: 'Español',
+        dir: 'ltr',
+    },
+    {
+        code: 'fr',
+        name: 'Français',
+        dir: 'ltr',
+    },
+    {
+        code: 'de',
+        name: 'Deutsch',
+        dir: 'ltr',
+    },
+]
