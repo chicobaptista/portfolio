95ef27f91857c407bc7baf1a78390fbbc8ecf718
Francisco Baptista <chico.baptista@gmail.com>
2018-11-20 17:20:37 -0200

    skills placeholders

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,8 +3,20 @@
     <h1>Skills</h1>
     <div class="row">
       <div class="col col-3">
-        <h1>Badge</h1>
-        <p>Skill name</p>
+        <img src="../assets/html.jpg">
+        <p>HTML</p>
+      </div>
+      <div class="col col-3">
+        <img src="../assets/css.jpg">
+        <p>CSS</p>
+      </div>
+      <div class="col col-3">
+        <img src="../assets/java.jpg">
+        <p>Javascript</p>
+      </div>
+      <div class="col col-3">
+        <img src="../assets/java.jpg">
+        <p>Angular</p>
       </div>
     </div>
   </div>
